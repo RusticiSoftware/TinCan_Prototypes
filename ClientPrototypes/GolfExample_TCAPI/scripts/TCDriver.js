cca909c47cb1898cb4a4cd0115c2332102af010c
Freddie O'Connell <freddie.oconnell@scorm.com>
2012-05-17 09:50:01 -0500

    Make the patch smarter than commenting out broken code.

--- ClientPrototypes/GolfExample_TCAPI/scripts/TCDriver.js
+++ ClientPrototypes/GolfExample_TCAPI/scripts/TCDriver.js
@@ -86,12 +86,10 @@ function XHR_request(lrs, url, method, data, auth, callback, ignore404, extraHea
         ieXDomain = true;
         ieModeRequest = TCDriver_GetIEModeRequest(method, url, headers, data);
         xhr = new XDomainRequest();
-        /*
-	if(console !== undefined) {
-		console.log(ieModeRequest.method + ", " + ieModeRequest.url);
-	}
-        */
-	xhr.open(ieModeRequest.method, ieModeRequest.url);
+    	if(typeof console !=== 'undefined') {
+	        console.log(ieModeRequest.method + ", " + ieModeRequest.url);
+	    }
+        xhr.open(ieModeRequest.method, ieModeRequest.url);
     }
     
     //Setup request callback
@@ -147,7 +145,7 @@ function XHR_request(lrs, url, method, data, auth, callback, ignore404, extraHea
 
 
 function TCDriver_Log(str){
-    if(console !== undefined){
+    if(console !=== 'undefined'){
         console.log(str);
     }
 }
