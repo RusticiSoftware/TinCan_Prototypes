d81f87e9e24b73a346cba2a525ed816ca50f1a33
Brian J. Miller <brianmiller@residualselfimage.com>
2012-12-12 09:32:47 -0600

    Update Golf Example with new TinCanJS supports .95

--- GolfExample_TCAPI/HavingFun/questions.js
+++ GolfExample_TCAPI/HavingFun/questions.js
@@ -1,23 +1,32 @@
-test.AddQuestion( new Question ("com.scorm.golfsamples.interactions.fun_1",
-                                "To make friends on the golf course, you should play really slowly.",
-                                QUESTION_TYPE_TF,
-                                null,
-                                false,
-                                "obj_havingfun")
-                );
- 
- test.AddQuestion( new Question ("com.scorm.golfsamples.interactions.fun_2",
-                                "Knickers indicate a refined sense of style.",
-                                QUESTION_TYPE_TF,
-                                null,
-                                false,
-                                "obj_havingfun")
-                );
-            
-test.AddQuestion( new Question ("com.scorm.golfsamples.interactions.fun_3",
-                                "You should take your score very seriously if you want to have a lot of fun on the course.",
-                                QUESTION_TYPE_TF,
-                                null,
-                                false,
-                                "obj_havingfun")
-                );
+test.AddQuestion(
+    new Question (
+        GolfExample.CourseActivity.id + "/GolfAssessment/interactions.fun_1",
+        "To make friends on the golf course, you should play really slowly.",
+        QUESTION_TYPE_TF,
+        null,
+        false,
+        "obj_havingfun"
+    )
+);
+
+test.AddQuestion(
+    new Question (
+        GolfExample.CourseActivity.id + "/GolfAssessment/interactions.fun_2",
+        "Knickers indicate a refined sense of style.",
+        QUESTION_TYPE_TF,
+        null,
+        false,
+        "obj_havingfun"
+    )
+);
+
+test.AddQuestion(
+    new Question (
+        GolfExample.CourseActivity.id + "/GolfAssessment/interactions.fun_3",
+        "You should take your score very seriously if you want to have a lot of fun on the course.",
+        QUESTION_TYPE_TF,
+        null,
+        false,
+        "obj_havingfun"
+    )
+);
--- GolfExample_TCAPI/scripts/common.js
+++ GolfExample_TCAPI/scripts/common.js
@@ -1,22 +1,30 @@
-GolfExample = { };
+GolfExample = {};
 
 GolfExample.CourseActivity = {
-    "id":"scorm.com/GolfExample_TCAPI",
-    "definition":{
-    	"type":"Course",
-    	"name":{"en-US":"Golf Example - Tin Can Course"},
-    	"description":{"en-US":"An overview of how to play the great game of golf."}
+    id: "http://tincanapi.com/GolfExample_TCAPI",
+    definition: {
+        type: "http://adlnet.gov/expapi/activities/course",
+        name: {
+            "en-US": "Golf Example - Tin Can Course"
+        },
+        description: {
+            "en-US": "An overview of how to play the great game of golf."
+        }
     }
 };
 
-GolfExample.getContext = function(parentActivityId){
+GolfExample.getContext = function(parentActivityId) {
     var ctx = {
-        "contextActivities":{
-            "grouping":{"id":GolfExample.CourseActivity.id}
+        contextActivities: {
+            grouping: {
+                id: GolfExample.CourseActivity.id
+            }
         }
     };
-    if(parentActivityId !== undefined && parentActivityId !== null){
-        ctx.contextActivities["parent"] = {"id":parentActivityId};
+    if (parentActivityId !== undefined && parentActivityId !== null) {
+        ctx.contextActivities.parent = {
+            id: parentActivityId
+        };
     }
     return ctx;
 };
